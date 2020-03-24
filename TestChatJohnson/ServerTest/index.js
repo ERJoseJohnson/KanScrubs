27fefcd9731d72c645ceb631b888599b6791caea
ERJoseJohnson <jose.johnson.jose@gmail.com>
2020-03-25 00:39:17 +0800

    Johnson chore: tried multiple ways to bypass CORS, failed

--- TestChatJohnson/ServerTest/index.js
+++ TestChatJohnson/ServerTest/index.js
@@ -8,12 +8,20 @@ var username;
 var pwd;
 var customerIncomingMessage;
 
-app.use(cors());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+app.options("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
 });
+
+app.use(cors());
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+//     next();
+// });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/static'));
@@ -31,7 +39,8 @@ app.post('/send', (req, res) => {
     res.end();
 });
 
-app.post('/customerMessage', (req, res) => {
+//app.options('*', cors());
+app.post('/customerMessage', cors(), (req, res) => {
     var data = req.body;
     console.log(data);
     customerIncomingMessage = data.incomingMessage;
