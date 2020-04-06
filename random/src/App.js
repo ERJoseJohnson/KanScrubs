8147571d7f8a3962b244d16a12d204c163ac4056
ERJoseJohnson <jose.johnson.jose@gmail.com>
2020-04-06 21:48:22 +0800

    Johnson feat: implemented login on frontend

--- random/src/App.js
+++ random/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import Chatbox from "./chat_ui/Chatbox";
 import "tachyons";
-import Login_n from "./login/Login_n" ; 
-
+import Login_n from "./login/Login_n";
+import axios from "axios";
 
 
 class App extends React.Component {
@@ -12,8 +12,8 @@ class App extends React.Component {
 
     this.state = {
       stored_value: "Your Message will appear hear",
-      loggedin : 0 , 
-      history: [], 
+      loggedin: 0,
+      history: [],
       userName: "",
       password: "",
       queryType: "",
@@ -27,52 +27,72 @@ class App extends React.Component {
     this.setState({ stored_value: event.target[0].value });
   };
 
-  login = () => {
-    this.setState({loggedin : 1}) ; 
-    console.log(this.state.loggedin) ;
-    console.log("logged in ") 
+  login = (username, password, queryType) => {
+    console.log(username);
+    const customerCreds = {
+      username: username,
+      password: password,
+      queryType: queryType
+    }
+    console.log(customerCreds)
+    let route = 'http://localhost:3001/login/'.concat(username)
+    console.log(route)
+
+    axios.post(route, customerCreds)
+      .then((response) => {
+        console.log(response.data);
+        let stateOfReq = response.data.success
+        let customerID = response.data.customer
+        let agentID = response.data.agent
+      }, (error) => {
+        console.log(error);
+      });
+
+    this.setState({ loggedin: 1 });
+
+    console.log("logged in ")
   };
 
-  signout = () =>{
-    window.location.reload(true) ; 
+  signout = () => {
+    window.location.reload(true);
   }
 
-    // Things to do before unloading/closing the tab
+  // Things to do before unloading/closing the tab
   doSomethingBeforeUnload = () => {
-      // Do something
-      window.alert("Are you sure you want to close ? ")
+    // Do something
+    window.alert("Are you sure you want to close ? ")
   }
 
   // Setup the `beforeunload` event listener
   setupBeforeUnloadListener = () => {
-      window.addEventListener("beforeunload", (ev) => {
-          ev.preventDefault();
+    window.addEventListener("beforeunload", (ev) => {
+      ev.preventDefault();
 
-          ev.returnValue("are you sure you want to leave")
-          return this.doSomethingBeforeUnload();
-      });
+      ev.returnValue("are you sure you want to leave")
+      return this.doSomethingBeforeUnload();
+    });
   };
 
   componentDidMount() {
-      // Activate the event listener
-      this.setupBeforeUnloadListener();
+    // Activate the event listener
+    this.setupBeforeUnloadListener();
   }
 
 
 
   test = () => {
-    if(this.state.loggedin == 0 ){
-      return (     
-      <div>
-        <Login_n userInfo={this.state} onSubmit={this.login}/>
-      </div>
-      ) ; 
-    }else {
+    if (this.state.loggedin == 0) {
       return (
-      <div>
-        <div className=""><Chatbox history={this.state.history} onMessage={this.updateHistory} signOut={this.signout} /></div>
-      </div>
-      ); 
+        <div>
+          <Login_n userInfo={this.state} onSubmit={this.login} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className=""><Chatbox history={this.state.history} onMessage={this.updateHistory} signOut={this.signout} /></div>
+        </div>
+      );
     }
 
   };
@@ -81,7 +101,7 @@ class App extends React.Component {
   render() {
     return (
 
-      this.test() 
+      this.test()
     );
   }
 }
